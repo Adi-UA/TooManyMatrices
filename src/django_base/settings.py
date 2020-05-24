82a08fed85640299e6f96d1e609ffff553487ef1
adi-ua <abanerjee@email.arizona.edu>
2020-05-24 11:16:48 -0700

    The BlogConfig class is now called TmmConfig. References updated.

--- src/django_base/settings.py
+++ src/django_base/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tmm.apps.BlogConfig',
+    'tmm.apps.TmmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/tmm/apps.py
+++ src/tmm/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
+class TmmConfig(AppConfig):
     name = 'tmm'
