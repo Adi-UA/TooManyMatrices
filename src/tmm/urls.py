05493783607f5bbb70e3fd58d623d5e140549445
Aditya Banerjee <55006642+Adi-UA@users.noreply.github.com>
2020-05-24 11:23:09 -0700

    Refactoring (#3)
    Refactored `django_project` to `django_base`
    All instances of `blog` have been changed to `tmm` (Too Many Matrices)
    Updated references and file tables to reflect the new structure.
    Updated migrations

--- src/tmm/urls.py
+++ src/tmm/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),  # redirects to home.html page
+    path('', views.home, name='tmm-home'),  # redirects to home.html page
     path('choose/', views.choose, name='choose'),
     path('add/', views.add, name='op_add'),  # redirects to op_addition.html
 ]
