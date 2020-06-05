fba8329aee1d550716c2c010d22f1194968e1c7f
dev-ved30 <vedshah.email@gmail.com>
2020-06-05 17:16:51 +0530

    * Added matrix bracket to the answers on the card. * Changed the matrix_to_list funtion to strip the last space in a row

--- src/tmm/utility.py
+++ src/tmm/utility.py
@@ -87,7 +87,7 @@ def matrix_to_list(m):
         s = ""
         for j in range(1, m.get_col_no() + 1):
             s += str(m.get_value(i, j)) + "    "
-        matrix_string.append(s)
+        matrix_string.append(s.rstrip())
     return matrix_string
 
 
